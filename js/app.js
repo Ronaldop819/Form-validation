470cf9c2b94e6c335288b5598bf96e1485e0ef36
Ronaldo Philippe Gomes Fortunato <ronaldo.fortunato@hcb.org.br>
2020-12-12 18:06:47 -0300

    inicio do app.js

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const usernameE1 = document.querySelector('#username');
+const emailE1 = document.querySelector('#email');
+const passwordE1 = document.querySelector('#password');
+const confirmPasswordE1 = document.querySelector('#confirm-password');
+
+const form = document.querySelector('#signup');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
+
+const isRequired = value => value === '' ? false : true;
+
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+const showError = (input, message) => {
+    const formField = input.parentElement;
+
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSucess = (input) => {
+    const formField = input.parentElement;
+
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
